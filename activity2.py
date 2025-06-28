e4119bc59c26f1299c6cd2e182f4463347b51490
Tisha0513 <tishavarshney13@gmail.com>
2025-06-28 18:04:34 +0700

    something

---
+++ activity2.py
@@ -0,0 +1,20 @@
+my_dict = {'name': 'Tisha','age': '14','nationality': 'Indian','learner': 'Student'}
+
+print(my_dict['name'])        
+print(my_dict.get('age'))    
+
+# Update any value
+my_dict['learner'] = 'teacher'
+print(my_dict)                
+
+# Add a new item
+my_dict['district'] = 'Indonesia'
+print(my_dict)               
+
+# Pop (remove) an item
+my_dict.pop('age')
+print(my_dict)               
+
+# Clear all items
+my_dict.clear()
+print(my_dict)              
