c02288e60f660e23b3c2ea054b656b8507e3e079
Tisha0513 <tishavarshney13@gmail.com>
2025-07-05 17:18:05 +0700

    1,2

---
+++ Module 8 lesson 3/activity.py
@@ -0,0 +1,10 @@
+class Student:
+    grade = 11
+    name = "Tisha"
+
+    def introduction(self):
+        print("Hello! I am", name)
+        print("Hi! I am a student of grade", self.grade)
+
+    ob = Student() #Accessing the class through object
+    ob.introduction() #Accessing a function through object
