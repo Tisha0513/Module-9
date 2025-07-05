88ddb96a18b75c01591881b1167ca2311ea76c02
Tisha0513 <tishavarshney13@gmail.com>
2025-07-05 17:47:20 +0700

    singing and dancing parrot help

--- Module 8 lesson 3/activity2.py
+++ Module 8 lesson 3/activity2.py
@@ -2,11 +2,22 @@ class Parrot:
 
     species = "bird"
 
-
+    #instance attributes
     def __init__(self, name,age):
         self.name = name
         self.age = age
 
+        #instance method
+        def sing(self, song):
+            return "{} sings {}".format(self.name, song)
+
+            #method
+            def dance(self):
+                return "{} is dancing".format(self.name)
+
 obj = Parrot("Christof", 7)
 
 print("{} is a {} and is {} years old".format(obj.name,obj.species,obj.age))
+
+print(obj.sing("Happy"))
+print(obj.dance())
